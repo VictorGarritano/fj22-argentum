81260efc85b95e1810482f8cadb73483fe9ec5fd
VictorGee <victorg@ufrj.br>
2014-09-28 21:20:50 -0300

    Testing CandleBuilder Class

--- src/main/java/br/com/caelum/argentum/testes/TestaCandleBuilder.java
+++ src/main/java/br/com/caelum/argentum/testes/TestaCandleBuilder.java
@@ -1,6 +1,6 @@
 package br.com.caelum.argentum.testes;
 
-import java.util.GregorianCalendar;
+import java.util.Calendar;
 import br.com.caelum.argentum.modelo.CandleBuilder;
 import br.com.caelum.argentum.modelo.CandleStick;
 
@@ -8,11 +8,14 @@ public class TestaCandleBuilder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+		Calendar hoje = Calendar.getInstance();
+
 		CandleStick candle = new CandleBuilder().comAbertura(40.5)
-			    .comFechamento(42.3).comMinimo(39.8).comMaximo(45.0)
-			    .comVolume(145234.20).comData(
-			    new GregorianCalendar(2014, 9, 27, 0, 0, 0)).geraCandle();
+				.comFechamento(42.3).comMinimo(39.8).comMaximo(45.0)
+				.comVolume(145234.20).comData(hoje).geraCandle();
+		
+		System.out.println(candle.toString());
 	}
 
 }
