7c7354cd89fab0b1e9be54f8d9e5331e2f1199e0
VictorGee <victorg@ufrj.br>
2014-09-28 22:13:53 -0300

    Adding TestsToDefineBetterTheSystem class
    The beggining

---
+++ src/main/java/br/com/caelum/argentum/testes/TestsToDefineBetterTheSystem.java
@@ -0,0 +1,10 @@
+package br.com.caelum.argentum.testes;
+
+public class TestsToDefineBetterTheSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
