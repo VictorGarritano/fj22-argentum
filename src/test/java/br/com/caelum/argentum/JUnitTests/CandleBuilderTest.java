33458f47764c149fa4c1809e1750c2bb4843fb0f
VictorGee <victorg@ufrj.br>
2014-10-01 14:07:22 -0300

    Upgrading classes

--- src/test/java/br/com/caelum/argentum/JUnitTests/CandleBuilderTest.java
+++ src/test/java/br/com/caelum/argentum/JUnitTests/CandleBuilderTest.java
@@ -1,10 +1,6 @@
 package br.com.caelum.argentum.JUnitTests;
 
-import java.util.Calendar;
-
 import org.junit.Test;
-
-import br.com.caelum.argentum.modelo.CandleBuilder;
 
 public class CandleBuilderTest {
 
