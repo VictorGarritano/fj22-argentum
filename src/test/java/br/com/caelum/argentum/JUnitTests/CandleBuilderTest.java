948d7a5a55b343598c3a8e3c67f23c50af78d385
VictorGee <victorg@ufrj.br>
2014-10-01 10:28:19 -0300

    Commiting some changes

---
+++ src/test/java/br/com/caelum/argentum/JUnitTests/CandleBuilderTest.java
@@ -0,0 +1,15 @@
+package br.com.caelum.argentum.JUnitTests;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import br.com.caelum.argentum.modelo.CandleBuilder;
+
+public class CandleBuilderTest {
+
+	@Test(expected=IllegalStateException.class)
+	public void geracaoDeCandleDeveTerTodosOsDadosNecessarios() {
+	
+	}
+}
