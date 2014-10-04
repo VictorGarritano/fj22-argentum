2a2fa21073c691128d69ce67ce76a6f587116103
VictorGee <victorg@ufrj.br>
2014-10-03 22:58:18 -0300

    Now I convinced myself that the tests are working the way they should work...
    And now I can sleep in peace and continue the optional exercises and
    pursue the project to GUI using javax.swing

---
+++ src/main/java/br/com/caelum/argentum/ui/EscolhedorDeXML.java
@@ -0,0 +1,39 @@
+package br.com.caelum.argentum.ui;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+
+import javax.swing.JOptionPane;
+
+import br.com.caelum.argentum.modelo.Negociacao;
+import br.com.caelum.argentum.reader.LeitorXML;
+
+public class EscolhedorDeXML {
+
+	public void escolhe() {
+		try {
+			JFileChooser chooser = new JFileChooser();
+			int retorno = chooser.showOpenDialog(null);
+
+			if (retorno == JFileChooser.APPROVE_OPTION) {
+				FileReader reader = new FileReader(chooser.getSelectedFile());
+				List<Negociacao> negocios = new LeitorXML().carrega(reader);
+
+				Negociacao primeiroNegocio = negocios.get(0);
+				String mensagem = "Primeiro negócio: "
+						+ primeiroNegocio.getPreco();
+				JOptionPane.showMessageDialog(null, mensagem);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new EscolhedorDeXML().escolhe();
+	}
+}
