fcbe2083ee0bbe75df14ec2c49354c6ace2a1a26
victorge/UBUNTU <victorg@ufrj.br>
2014-10-09 14:15:19 -0300

    Adding the SerieTemporal and GeradorDeSerie classes

---
+++ src/main/java/br/com/caelum/argentum/modelo/GeradorDeSerie.java
@@ -0,0 +1,23 @@
+package br.com.caelum.argentum.modelo;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class GeradorDeSerie {
+	/**
+	  * Serve para ajudar a fazer os testes.
+	  * 
+	  * Recebe uma sequência de valores e cria candles com abertura, fechamento, 
+	  * minimo e maximo iguais, mil de volume e data de hoje. Finalmente, devolve
+	  * tais candles encapsuladas em uma Serie Temporal.
+	  **/
+	 public static SerieTemporal criaSerie(double... valores) {
+	   List<CandleStick> candles = new ArrayList<CandleStick>();
+	   for (double d : valores) {
+	     candles.add(new CandleStick(d, d, d, d, 1000, 
+	                     Calendar.getInstance()));
+	   }
+	   return new SerieTemporal(candles);
+	 }
+}
